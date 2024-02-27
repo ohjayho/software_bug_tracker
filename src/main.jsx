0a5552ff1004d28ceec21212276773438c6eb5d0
Jay Ho Oh <ohjayhoi@gmail.com>
2024-02-28 00:55:10 +0900

    feat: add route paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import LoginForm from "./Components/LoginForm.jsx";
+import DashBoard from "./Components/DashBoard.jsx";
+import Create from "./Components/Create.jsx";
+import About from "./Components/About.jsx";
+import Issue from "./Components/Issue.jsx";
+import Footer from "./Components/Footer.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<DashBoard />}></Route>
+        <Route path="/dashboard/issue/:id" element={<Issue />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/footer" element={<Footer />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
