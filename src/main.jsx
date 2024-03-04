736a7ed664e35fc0c612872b0d5ff49cd0d1a2f2
Jay Ho Oh <ohjayhoi@gmail.com>
2024-03-04 23:48:58 +0900

    refactor: move the code from main.jsx to App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
 import "./index.css";
-import LoginForm from "./Components/LoginForm.jsx";
-import DashBoard from "./Components/DashBoard.jsx";
-import Create from "./Components/Create.jsx";
-import About from "./Components/About.jsx";
-import Issue from "./Components/Issue.jsx";
-import Footer from "./Components/Footer.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/dashboard" element={<DashBoard />}></Route>
-        <Route path="/dashboard/issue/:id" element={<Issue />} />
-        <Route path="/create" element={<Create />} />
-        <Route path="/footer" element={<Footer />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
