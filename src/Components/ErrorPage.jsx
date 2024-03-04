7d2bb961ce33010b4b6ca613c9232d2847281cbe
Jay Ho Oh <ohjayhoi@gmail.com>
2024-03-04 23:51:28 +0900

    design: change the content of ErrorPage

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -1,15 +1,8 @@
-import { useRouteError } from "react-router-dom";
-
 export default function ErrorPage() {
-  const error = useRouteError();
-  console.log(error);
   return (
-    <div id="error-page">
+    <div>
       <h1>oops!</h1>
       <p>미안하다 에러다</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
     </div>
   );
 }
