793b1e97bd21d57dbb5f1cc8e1e3e84f6066c120
Jay Ho Oh <ohjayhoi@gmail.com>
2024-03-04 23:44:42 +0900

    refactor: move the code from main.jsx to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import "./App.css";
-import LoginForm from "./Components/LoginForm";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginForm from "./Components/LoginForm.jsx";
+import DashBoard from "./Components/DashBoard.jsx";
+import Create from "./Components/Create.jsx";
+import About from "./Components/About.jsx";
+import Issue from "./Components/Issue.jsx";
+import ErrorPage from "./Components/ErrorPage.jsx";
+import Header from "./Components/Header.jsx";
+import Footer from "./Components/Footer.jsx";
 
 function App() {
   return (
-    <>
-      <LoginForm />
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<DashBoard />}></Route>
+        <Route path="/dashboard/issue/:id" element={<Issue />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
