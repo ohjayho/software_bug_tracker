91d40149eaf7cd1557814d0f221ebcb894cfae14
jayho oh <>
2024-03-27 16:37:21 +0900

    chore: re-route auth page

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Create from "./Components/pages/create/Create.jsx";
 import About from "./Components/pages/about/About.jsx";
 import ErrorPage from "./Components/ErrorPage.jsx";
 import Issue from "./Components/pages/issue/Issue.jsx";
-import Login from "./Components/pages/Login/Login.jsx";
 import Auth from "./Components/pages/auth/Auth.jsx";
 import MainLayout from "./Components/MainLayout.jsx";
 
@@ -13,15 +12,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<Auth />} />
         <Route element={<MainLayout />}>
-          <Route path="/" element={<Login />} />
           <Route path="/about" element={<About />} />
           <Route path="/dashboard" element={<DashBoard />} />
           <Route path="/dashboard/issue/:id" element={<Issue />} />
           <Route path="/create" element={<Create />} />
           <Route path="*" element={<ErrorPage />} />
         </Route>
-        <Route path="/login" element={<Auth />} />
       </Routes>
     </BrowserRouter>
   );
