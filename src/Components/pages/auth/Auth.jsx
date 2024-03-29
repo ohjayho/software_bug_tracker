86129453fba6109c212de2596d9544a48580dd66
Jay Ho Oh <ohjayhoi@gmail.com>
2024-03-30 00:27:09 +0900

    chore: merge Login component into Auth component

--- src/Components/pages/auth/Auth.jsx
+++ src/Components/pages/auth/Auth.jsx
@@ -1,11 +1,45 @@
 import "./Auth.css";
-import Login from "./Login/Login";
+import { useState } from "react";
+import SignUpForm from "./components/SignUpForm";
+import LoginForm from "./components/LoginForm";
 
 const Auth = () => {
+  const [register, setRegister] = useState(false);
   return (
     <div className="auth_container">
       <h1 className="title_auth">Software Bug Tracker</h1>
-      <Login />
+      <div className="login_container">
+        {register ? (
+          <SignUpForm setRegister={setRegister} />
+        ) : (
+          <LoginForm setRegister={setRegister} />
+        )}
+        {register ? (
+          <div className="form_msg_container">
+            <h5 className="form_msg login_msg">Already have an account?</h5>
+            <h5
+              className="signup_msg signup_click"
+              onClick={() => {
+                setRegister(false);
+              }}
+            >
+              Login!
+            </h5>
+          </div>
+        ) : (
+          <div className="form_msg_container">
+            <h5 className="form_msg signup_msg">Don't have an account?</h5>
+            <h5
+              className="signup_msg signup_click"
+              onClick={() => {
+                setRegister(true);
+              }}
+            >
+              Sign up!
+            </h5>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
