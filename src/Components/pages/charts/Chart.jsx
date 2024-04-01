9c22a323d2a35986bf28f1a5a747de07e50c7cac
Jay Ho Oh <ohjayhoi@gmail.com>
2024-04-01 14:08:34 +0900

    feat: add Chart Component for the dashboard

---
+++ src/Components/pages/charts/Chart.jsx
@@ -0,0 +1,44 @@
+import { Pie } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+import "./Chart.css";
+
+ChartJS.register(ArcElement, Tooltip, ChartDataLabels, Legend);
+
+const data = {
+  labels: ["Critical", "Urgent", "Major"],
+  datasets: [
+    {
+      label: "Tickets by Severity",
+      data: [12, 19, 3],
+      backgroundColor: [
+        "rgb(255, 99, 132)",
+        "rgb(54, 162, 235)",
+        "rgb(255, 206, 86)"
+      ]
+    }
+  ]
+};
+
+const options = {
+  plugins: {
+    legend: {
+      position: "right"
+    }
+  }
+};
+
+const option = {};
+
+const Chart = () => {
+  return (
+    <div className="chart_container">
+      <div className="header_chart">
+        <h1 className="title_chart">Tickets by Type</h1>
+      </div>
+      <Pie data={data} options={options} />
+    </div>
+  );
+};
+
+export default Chart;
