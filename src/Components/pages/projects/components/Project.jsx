88ba2f3a7e1c2db9baece26f022b7a7ca4ad29c5
Jay Ho Oh <ohjayhoi@gmail.com>
2024-04-01 16:09:02 +0900

    feat: connect Project with localStorage. get the localStorage object from props

--- src/Components/pages/projects/components/Project.jsx
+++ src/Components/pages/projects/components/Project.jsx
@@ -1,12 +1,16 @@
-const Project = ({ props }) => {
+const Project = ({ project }) => {
   console.log("hey");
   return (
     <tr>
-      <td>{props.project}</td>
-      <td>{props.description}</td>
+      <td>{project.project_name}</td>
+      <td>{project.project_description}</td>
       <td>
         <div className="contributors_table">
-          {props.contributors}
+          <div className="members_contributors">
+            {project.project_members.map((member) => {
+              return <div className="name_members">{member}</div>;
+            })}
+          </div>
           <h3 className="dots_contributors">:</h3>
         </div>
       </td>
