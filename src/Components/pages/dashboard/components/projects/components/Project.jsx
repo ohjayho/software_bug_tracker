554917415ac1671a082b2a70279c10f37b1a73c3
Jay Ho Oh <ohjayhoi@gmail.com>
2024-04-02 01:12:21 +0900

    chore: change the name of the property

--- src/Components/pages/dashboard/components/projects/components/Project.jsx
+++ src/Components/pages/dashboard/components/projects/components/Project.jsx
@@ -1,15 +1,16 @@
 const Project = ({ project }) => {
-  console.log("hey");
+  console.log(project);
   return (
     <tr>
-      <td>{project.project_name}</td>
-      <td>{project.project_description}</td>
+      <td>{project.name}</td>
+      <td>{project.description}</td>
       <td>
         <div className="contributors_table">
           <div className="members_contributors">
-            {project.project_members.map((member) => {
+            {/* {project.members.map((member) => {
               return <div className="name_members">{member}</div>;
-            })}
+            })} */}
+            {project.members}
           </div>
           <h3 className="dots_contributors">:</h3>
         </div>
