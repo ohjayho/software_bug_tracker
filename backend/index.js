9c49f74eb3a436249d73d7f8bdddd71769cd07c1
Jay Ho Oh <ohjayhoi@gmail.com>
2024-04-03 19:11:28 +0900

    feat: insert data into MySQL

--- backend/index.js
+++ backend/index.js
@@ -19,12 +19,32 @@ app.get("/", (req, res) => {
 });
 
 app.get("/projects", (req, res) => {
-  const q = "SELECT * FROM projects";
+  const q = "SELECT * FROM projects ORDER BY created_at DESC;";
   db.query(q, (err, data) => {
-    console.log("hey", err, data);
     if (err) return res.json(err);
     return res.json(data);
   });
+});
+
+app.post("/projects", (req, res) => {
+  const q = "INSERT INTO projects (id, name, description , members) VALUES (?)";
+
+  const { id, name, description, members } = req.body;
+  const values = [id, name, description, members];
+
+  db.query(q, [values], (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
+  });
+
+  // db.query(q, (err, data) => {
+  //   console.log("abc", err, data);
+  //   if (err) return res.json(err);
+  //   return res.json(data);
+  // });
 });
 
 app.listen(8800, () => {
