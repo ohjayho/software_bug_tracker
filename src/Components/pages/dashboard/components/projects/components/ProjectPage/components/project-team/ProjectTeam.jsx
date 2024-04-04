bf04f84dc017bbbda680667117a011faaa305a66
Jay Ho Oh <ohjayhoi@gmail.com>
2024-04-04 17:04:18 +0900

    feat: add a component that shows members of a project

---
+++ src/Components/pages/dashboard/components/projects/components/ProjectPage/components/project-team/ProjectTeam.jsx
@@ -0,0 +1,40 @@
+import "../../../../../../components.css";
+
+const ProjectTeam = () => {
+  return (
+    <div className="project_team_container border_shadow_component components_container">
+      <div className="header_project_team header_components">
+        <h1 className="title_project_team title_header">Team</h1>
+        <button className="btn_new_member btn_new">New member</button>
+      </div>
+      <table className="table_team table_components">
+        <thead>
+          <tr>
+            <th>NAME</th>
+            <th>EMAIL</th>
+            <th>PHONE</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>name</td>
+            <td>email</td>
+            <td>
+              <div className="last_column_table">
+                <div className="members_contributors">phone</div>
+                <h3 className="menu_dots">:</h3>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div className="btns_pagination_container">
+        <div className="btns_pagination prev_btn_pages">{`<`}</div>
+        <div className="btns_pagination num_btn_pages">1</div>
+        <div className="btns_pagination next_btn_pages">{`>`}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectTeam;
