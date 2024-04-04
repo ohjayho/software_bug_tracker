77aab95f58b44fcb46d4bceab135d9e408287fac
Jay Ho Oh <ohjayhoi@gmail.com>
2024-04-04 17:00:29 +0900

    feat: add ProjectPage component for a detailed page for a project

---
+++ src/Components/pages/dashboard/components/projects/components/ProjectPage/ProjectPage.jsx
@@ -0,0 +1,11 @@
+import ProjectTeam from "./components/project-team/ProjectTeam";
+
+const ProjectPage = () => {
+  return (
+    <div className="project_page_container">
+      <ProjectTeam />
+    </div>
+  );
+};
+
+export default ProjectPage;
