fe339d96c956cb2f169c1efe60723d0420eef93d
Jay Ho Oh <ohjayhoi@gmail.com>
2024-04-08 01:07:09 +0900

    feat: add SQL queries for fetching data from MySQL and enable multipleStatements to send multiple queries concurrently

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,8 @@ const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "0117",
-  database: "software_bug_tracker"
+  database: "software_bug_tracker",
+  multipleStatements: true
 });
 
 app.use(express.json());
@@ -26,11 +27,37 @@ app.get("/projects", (req, res) => {
   });
 });
 
-app.post("/projects", (req, res) => {
-  const q = "INSERT INTO projects (id, name, description , members) VALUES (?)";
+app.get("/users", (req, res) => {
+  const q = "SELECT * FROM users";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
 
-  const { id, name, description, members } = req.body;
-  const values = [id, name, description, members];
+app.get("/dashboard/project/:id", (req, res) => {
+  const members = `SELECT member FROM project_members WHERE project_id='${req.params.id}';`;
+  const tickets = `SELECT id, title, description, author FROM project_tickets WHERE project_id='${req.params.id}'`;
+  db.query(members + tickets, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.get("/dashboard/project/:id/project_tickets", (req, res) => {
+  const tickets = `SELECT * FROM tickets_info WHERE project_id='${req.params.id}'`;
+  db.query(tickets, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/projects", (req, res) => {
+  const q =
+    "INSERT INTO projects (id, name, description , author_id) VALUES (?)";
+
+  const { id, name, description, author_id } = req.body;
+  const values = [id, name, description, author_id];
 
   db.query(q, [values], (err, data) => {
     if (err) {
@@ -39,12 +66,22 @@ app.post("/projects", (req, res) => {
     }
     return res.json(data);
   });
+});
 
-  // db.query(q, (err, data) => {
-  //   console.log("abc", err, data);
-  //   if (err) return res.json(err);
-  //   return res.json(data);
-  // });
+app.post("/users", (req, res) => {
+  const q = "INSERT INTO users (id, pw) VALUES (?)";
+
+  const { id, pw } = req.body;
+  const values = [id, pw];
+
+  db.query(q, [values], (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
+  });
+  console.log("successfully created!");
 });
 
 app.listen(8800, () => {
