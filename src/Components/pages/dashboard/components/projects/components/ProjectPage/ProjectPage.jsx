3f6697d0d27a13333a07dbfb137125458c00027b
Jay Ho Oh <ohjayhoi@gmail.com>
2024-04-07 21:14:16 +0900

    feat: fetch information of projects from MySQL and send them as props to components

--- src/Components/pages/dashboard/components/projects/components/ProjectPage/ProjectPage.jsx
+++ src/Components/pages/dashboard/components/projects/components/ProjectPage/ProjectPage.jsx
@@ -1,9 +1,44 @@
+import { useParams } from "react-router-dom";
+import "./ProjectPage.css";
 import ProjectTeam from "./components/project-team/ProjectTeam";
+import ProjectTickets from "./components/project-tickets/ProjectTickets";
+import SelectedTicket from "./components/project-tickets/components/SelectedTicket";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const ProjectPage = () => {
+  const { id } = useParams();
+  const [projectInfo, setProjectInfo] = useState({});
+  const [selectedTicket, setSelectedTicket] = useState(false);
+
+  useEffect(() => {
+    const fetchProjectInfo = async () => {
+      try {
+        const res = await axios.get(
+          `http://localhost:8800/dashboard/project/${id}`
+        );
+        setProjectInfo(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchProjectInfo();
+  }, []);
+
   return (
     <div className="project_page_container">
-      <ProjectTeam />
+      <div className="upper_section_project_page">
+        {projectInfo.length && <ProjectTeam team={projectInfo[0]} />}
+        {projectInfo.length && (
+          <ProjectTickets
+            tickets={projectInfo[1]}
+            setSelectedTicket={setSelectedTicket}
+          />
+        )}
+      </div>
+      <div className="lower_section_project_page">
+        <SelectedTicket selectedTicket={selectedTicket} />
+      </div>
     </div>
   );
 };
