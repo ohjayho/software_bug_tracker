e4f34eca150ef46154bade840109798dcaefe380
Jay Ho Oh <ohjayhoi@gmail.com>
2024-04-09 00:37:36 +0900

    refactor: pass title props to Chart component to render the title differently

--- src/Components/pages/dashboard/DashBoard.jsx
+++ src/Components/pages/dashboard/DashBoard.jsx
@@ -5,12 +5,12 @@ import Charts from "./components/charts/Chart";
 const DashBoard = () => {
   return (
     <div className="dashboard_container">
-      <h1 className="header_dashboard">DASHBOARD</h1>
+      <h1 className="header_dashboard title_container">DASHBOARD</h1>
       <Projects />
       <div className="charts_container">
-        <Charts />
-        <Charts />
-        <Charts />
+        <Charts title={"Type"} />
+        <Charts title={"Status"} />
+        <Charts title={"Priority"} />
       </div>
     </div>
   );
