12dacbe3692424e51e797de5328d04df289c8a93
Jay Ho Oh <ohjayhoi@gmail.com>
2024-04-09 16:13:35 +0900

    feat, style: get contributors from project_members table and make whole row of the table clickable by adding useNavigate()

--- src/Components/pages/dashboard/components/projects/components/Project.jsx
+++ src/Components/pages/dashboard/components/projects/components/Project.jsx
@@ -1,22 +1,42 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./Project.css";
 
 const Project = ({ project }) => {
+  const [contributors, setContributors] = useState([]);
+
+  useEffect(() => {
+    const getContributors = async () => {
+      try {
+        const res = await axios.get(
+          `http://localhost:8800/project_members/${project.id}`
+        );
+        setContributors(res.data);
+        console.log("컨트리", res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getContributors();
+  }, []);
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/dashboard/project/${project.id}`);
+  };
+
   return (
-    <tr>
-      <td>
-        <Link to={`/dashboard/project/${project.id}`}>{project.name}</Link>
-      </td>
-      <td>
-        <Link to={`/dashboard/project/${project.id}`}>
-          {project.description}
-        </Link>
-      </td>
+    <tr className="project_table" onClick={handleClick}>
+      <td>{project.name}</td>
+      <td>{project.description}</td>
       <td>
         <div className="contributors_table last_column_table">
           <div className="members_contributors">
-            <Link to={`/dashboard/project/${project.id}`}>
-              {project.members}
-            </Link>
+            {contributors.map((contributor) => {
+              return <div key={contributor.member}>{contributor.member}</div>;
+            })}
           </div>
           <h3 className="menu_dots">:</h3>
         </div>
