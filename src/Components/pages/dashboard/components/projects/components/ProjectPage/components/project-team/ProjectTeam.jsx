6f2e0e13ab75e6982922ea4350ecfad2d9de4c18
Jay Ho Oh <ohjayhoi@gmail.com>
2024-04-09 23:34:37 +0900

    feat: add a modal for managing members of a project

--- src/Components/pages/dashboard/components/projects/components/ProjectPage/components/project-team/ProjectTeam.jsx
+++ src/Components/pages/dashboard/components/projects/components/ProjectPage/components/project-team/ProjectTeam.jsx
@@ -1,43 +1,61 @@
 import "../../../../../../components.css";
+import { useState } from "react";
+import { createPortal } from "react-dom";
+import TeamModal from "./components/TeamModal";
 
 const ProjectTeam = ({ team }) => {
+  const [teamModalOpen, setTeamModalOpen] = useState(false);
   return (
-    <div className="project_team_container border_shadow_component components_container">
-      <div className="header_project_team header_components">
-        <h1 className="title_project_team title_header">Team</h1>
-        <button className="btn_new_member btn_new">New member</button>
+    <>
+      {teamModalOpen &&
+        createPortal(
+          <TeamModal setTeamModalOpen={setTeamModalOpen} />,
+          document.body
+        )}
+      <div className="project_team_container border_shadow_component components_container">
+        <div className="header_project_team header_components">
+          <h1 className="title_project_team title_header">Team</h1>
+          <button
+            className="btn_new_member btn_new"
+            onClick={() => {
+              setTeamModalOpen(true);
+            }}
+          >
+            New member
+          </button>
+        </div>
+        <table className="table_team table_components">
+          <thead>
+            <tr>
+              <th>NAME</th>
+              <th>EMAIL</th>
+              <th>PHONE</th>
+            </tr>
+          </thead>
+          <tbody>
+            {team.map((mb) => {
+              return (
+                <tr key={mb.member}>
+                  <td>{mb.member}</td>
+                  <td>{mb.email}</td>
+                  <td>
+                    <div className="last_column_table">
+                      <div className="members_contributors">{mb.phone}</div>
+                      <h3 className="menu_dots">:</h3>
+                    </div>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <div className="btns_pagination_container">
+          <div className="btns_pagination prev_btn_pages">{`<`}</div>
+          <div className="btns_pagination num_btn_pages">1</div>
+          <div className="btns_pagination next_btn_pages">{`>`}</div>
+        </div>
       </div>
-      <table className="table_team table_components">
-        <thead>
-          <tr>
-            <th>NAME</th>
-            <th>EMAIL</th>
-            <th>PHONE</th>
-          </tr>
-        </thead>
-        <tbody>
-          {team.map((mb) => {
-            return (
-              <tr key={mb.member}>
-                <td>{mb.member}</td>
-                <td>{mb.email}</td>
-                <td>
-                  <div className="last_column_table">
-                    <div className="members_contributors">{mb.phone}</div>
-                    <h3 className="menu_dots">:</h3>
-                  </div>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <div className="btns_pagination_container">
-        <div className="btns_pagination prev_btn_pages">{`<`}</div>
-        <div className="btns_pagination num_btn_pages">1</div>
-        <div className="btns_pagination next_btn_pages">{`>`}</div>
-      </div>
-    </div>
+    </>
   );
 };
 
