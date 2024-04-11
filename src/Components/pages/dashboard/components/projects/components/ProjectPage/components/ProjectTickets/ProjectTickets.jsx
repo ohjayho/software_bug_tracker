01dc6af2b7f62fb69cf61bd8246be05f9088f5f5
Jay Ho Oh <ohjayhoi@gmail.com>
2024-04-11 12:15:04 +0900

    chore: change the name of the folder of TicketModal

--- src/Components/pages/dashboard/components/projects/components/ProjectPage/components/ProjectTickets/ProjectTickets.jsx
+++ src/Components/pages/dashboard/components/projects/components/ProjectPage/components/ProjectTickets/ProjectTickets.jsx
@@ -5,7 +5,7 @@ import "./ProjectTickets.css";
 import { useState, useEffect } from "react";
 import { createPortal } from "react-dom";
 import axios from "axios";
-import TicketModal from "./components/ticket-modal/TicketModal";
+import TicketModal from "./components/TicketModal/TicketModal";
 
 const ProjectTickets = ({ tickets, setSelectedTicket }) => {
   const [storedTickets, setStoredTickets] = useState([]);
