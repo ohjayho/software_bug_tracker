680e5ad844f396fa5a89b2c67a216dc81e6bd49b
Jay Ho Oh <ohjayhoi@gmail.com>
2024-04-13 01:02:45 +0900

    feat: show current members of a project and add new members to a project

--- src/Components/pages/dashboard/components/projects/components/ProjectPage/components/ProjectTeam/components/TeamModal.jsx
+++ src/Components/pages/dashboard/components/projects/components/ProjectPage/components/ProjectTeam/components/TeamModal.jsx
@@ -1,9 +1,50 @@
 import "./TeamModal.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const TeamModal = ({ setTeamModalOpen }) => {
+const TeamModal = ({ notSelectedTeam, setTeamModalOpen, id }) => {
+  const [addMembers, setAddMembers] = useState([]);
+  const [newMembers, setNewMembers] = useState([]);
+  const [initialRender, setInitialRender] = useState(true);
+
+  const handleMembers = (e) => {
+    let selected = [];
+    console.log("모달id", id);
+    Array.prototype.forEach.call(e.target.options, function (opt) {
+      if (opt.selected) {
+        selected = [...selected, [id, opt.value]];
+      }
+    });
+    setAddMembers(selected);
+    console.log(selected);
+  };
+
+  useEffect(() => {
+    if (initialRender) {
+      setInitialRender(false);
+      return;
+    }
+
+    console.log("뉴", newMembers);
+
+    const uploadMembers = async () => {
+      try {
+        await axios.post(`http://localhost:8800/project_members`, newMembers);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    uploadMembers();
+  }, [newMembers]);
+
+  const teamSubmitHandle = () => {
+    setNewMembers(addMembers);
+    console.log("애드멤버", addMembers);
+  };
+
   return (
     <div className="modal_container">
-      <form className="modal">
+      <form onSubmit={teamSubmitHandle} className="modal">
         <div className="header_modal">
           <h1 className="title_modal">Add Member</h1>
           <h1
@@ -22,7 +63,16 @@ const TeamModal = ({ setTeamModalOpen }) => {
               className="select_modal"
               multiple
               required
-            ></select>
+              onChange={(e) => handleMembers(e)}
+            >
+              {notSelectedTeam.map((nst) => {
+                return (
+                  <option key={nst.id} value={nst.id}>
+                    {nst.id}
+                  </option>
+                );
+              })}
+            </select>
           </div>
         </div>
         <div className="footer_modal">
