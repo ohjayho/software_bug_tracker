ee8c84cea6646d9ef7322886c56b45c7024def30
Jay Ho Oh <ohjayhoi@gmail.com>
2024-04-13 00:52:31 +0900

    feat: add delete feature to the dropdown menu of a project

--- src/Components/pages/dashboard/components/projects/components/DropDownMenu/DropDownMenu.jsx
+++ src/Components/pages/dashboard/components/projects/components/DropDownMenu/DropDownMenu.jsx
@@ -1,13 +1,25 @@
 import "./DropDownMenu.css";
+import axios from "axios";
 
-const DropDownMenu = () => {
+const DropDownMenu = ({ project_id }) => {
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`http://localhost:8800/projects/${project_id}`);
+    } catch (err) {
+      console.log(err);
+    }
+    location.reload();
+  };
+
   return (
     <div className="drop_down_menu_container border_shadow_component">
       <div className="edit_container">
         <div className="btn_edit btns_dropdown">Edit</div>
       </div>
       <div className="delete_container">
-        <div className="btn_delete btns_dropdown">Delete</div>
+        <div className="btn_delete btns_dropdown" onClick={handleDelete}>
+          Delete
+        </div>
       </div>
     </div>
   );
