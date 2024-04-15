5aa6ea0f2c53da6ff26e762b8da4bb13f6b73b6c
Jay Ho Oh <ohjayhoi@gmail.com>
2024-04-15 09:46:42 +0900

    refactor: declare team state variable in ProjectPage to share a list of a team with ProjectTeam and ProjectTickets

--- src/Components/pages/dashboard/components/projects/components/ProjectPage/ProjectPage.jsx
+++ src/Components/pages/dashboard/components/projects/components/ProjectPage/ProjectPage.jsx
@@ -8,13 +8,20 @@ import { useState } from "react";
 const ProjectPage = () => {
   const { id } = useParams();
   const [selectedTicket, setSelectedTicket] = useState(false);
+  // to pass team data to both ProjectTeam and ProjectTickets, declare useState here.
+  // Actual assigning is happening in ProjectTeam in the useEffect
+  const [team, setTeam] = useState([]);
 
   return (
     <div className="project_page_container">
       <h1 className="header_project_page title_container">PROJECT</h1>
       <div className="upper_section_project_page">
-        <ProjectTeam id={id} />
-        <ProjectTickets id={id} setSelectedTicket={setSelectedTicket} />
+        <ProjectTeam id={id} team={team} setTeam={setTeam} />
+        <ProjectTickets
+          id={id}
+          setSelectedTicket={setSelectedTicket}
+          team={team[0]}
+        />
       </div>
       <div className="lower_section_project_page">
         <SelectedTicket selectedTicket={selectedTicket} />
