8549b40f0914919a293b634d753ac509e269d367
Jay Ho Oh <ohjayhoi@gmail.com>
2024-04-15 16:40:45 +0900

    refactor: add Ticket component to render selected ticket

---
+++ src/Components/pages/dashboard/components/projects/components/ProjectPage/components/ProjectTickets/components/Ticket/Ticket.jsx
@@ -0,0 +1,25 @@
+const Ticket = ({ ticket, setSelectedTicket, setSelectedTicketOpen }) => {
+  const handleTicketClick = () => {
+    setSelectedTicket(ticket);
+    setSelectedTicketOpen((so) => !so);
+  };
+
+  return (
+    <tr
+      onClick={handleTicketClick}
+      className="ticket_table"
+      key={ticket.ticket_id}
+    >
+      <td>{ticket.title}</td>
+      <td>{ticket.description}</td>
+      <td>
+        <div className="last_column_table">
+          <div className="members_contributors">{ticket.author}</div>
+          <h3 className="menu_dots">:</h3>
+        </div>
+      </td>
+    </tr>
+  );
+};
+
+export default Ticket;
