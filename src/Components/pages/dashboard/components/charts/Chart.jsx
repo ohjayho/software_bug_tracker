87a1708d4097abe41345a8628d9d6b87e724f6a0
Jay Ho Oh <ohjayhoi@gmail.com>
2024-04-17 18:57:10 +0900

    feat: display ticket statistics using charts

--- src/Components/pages/dashboard/components/charts/Chart.jsx
+++ src/Components/pages/dashboard/components/charts/Chart.jsx
@@ -1,45 +1,91 @@
 import { Pie } from "react-chartjs-2";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  Colors
+} from "chart.js";
 import ChartDataLabels from "chartjs-plugin-datalabels";
 import "./Chart.css";
 
-ChartJS.register(ArcElement, Tooltip, ChartDataLabels, Legend);
+ChartJS.register(ArcElement, Tooltip, ChartDataLabels, Legend, Colors);
+ChartJS.register({
+  id: "NoData",
+  afterDraw: (chart) => {
+    if (chart.data.datasets[0].data.length == 0) {
+      let ctx = chart.ctx;
+      let width = chart.width;
+      let height = chart.height;
 
-const data = {
-  labels: ["Critical", "Urgent", "Major"],
-  datasets: [
-    {
-      label: "Tickets by Severity",
-      data: [12, 19, 3],
-      backgroundColor: [
-        "rgb(255, 99, 132)",
-        "rgb(54, 162, 235)",
-        "rgb(255, 206, 86)"
-      ]
-    }
-  ]
-};
-
-const options = {
-  plugins: {
-    legend: {
-      position: "right"
-    },
-    datalabels: {
-      color: "#ffffff"
+      chart.clear();
+      ctx.save();
+      ctx.textAlign = "center";
+      ctx.texBaseline = "middle";
+      ctx.fillText("No ticket to display", width / 2, height / 2);
+      ctx.restore();
     }
   }
-};
+});
 
-const option = {};
+const Chart = ({ title, tickets }) => {
+  const type_data = {
+    labels: tickets[0].map((ticket) => ticket.type),
+    datasets: [
+      {
+        label: "Tickets by Type",
+        data: tickets[0].map((ticket) => ticket.cnt)
+      }
+    ]
+  };
+  const status_data = {
+    labels: tickets[1].map((ticket) => ticket.status),
+    datasets: [
+      {
+        label: "Tickets by Status",
+        data: tickets[1].map((ticket) => ticket.cnt)
+      }
+    ]
+  };
+  const priority_data = {
+    labels: tickets[2].map((ticket) => ticket.priority),
+    datasets: [
+      {
+        label: "Tickets by Priority",
+        data: tickets[2].map((ticket) => ticket.cnt)
+      }
+    ]
+  };
 
-const Chart = ({ title }) => {
+  const options = {
+    plugins: {
+      legend: {
+        position: "right"
+      },
+      datalabels: {
+        color: "#ffffff"
+      }
+    }
+  };
+
+  let data;
+
+  if (title == "Type") {
+    data = type_data;
+  }
+  if (title == "Status") {
+    data = status_data;
+  }
+  if (title == "Priority") {
+    data = priority_data;
+  }
+
   return (
     <div className="chart_container border_shadow_component">
       <div className="header_chart">
         <h1 className="title_chart">{`Tickets by ${title}`}</h1>
       </div>
-      <Pie data={data} options={options} />
+      {<Pie data={data} options={options} />}
     </div>
   );
 };
