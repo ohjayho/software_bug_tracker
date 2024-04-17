3f70524a28b5d0af91ec8f05dccbb795f85e83f5
Jay Ho Oh <ohjayhoi@gmail.com>
2024-04-17 19:00:55 +0900

    feat: fetch tickets data and use them for displaying their statistics

--- src/Components/pages/dashboard/DashBoard.jsx
+++ src/Components/pages/dashboard/DashBoard.jsx
@@ -1,16 +1,32 @@
 import "./DashBoard.css";
 import Projects from "./components/projects/Projects";
 import Charts from "./components/charts/Chart";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const DashBoard = () => {
+  const [tickets, setTickets] = useState([]);
+
+  useEffect(() => {
+    const getTickets = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/project_tickets");
+        setTickets(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getTickets();
+  }, []);
+
   return (
     <div className="dashboard_container">
       <h1 className="header_dashboard title_container">DASHBOARD</h1>
       <Projects />
       <div className="charts_container">
-        <Charts title={"Type"} />
-        <Charts title={"Status"} />
-        <Charts title={"Priority"} />
+        {tickets.length > 0 && <Charts title={"Type"} tickets={tickets} />}
+        {tickets.length > 0 && <Charts title={"Status"} tickets={tickets} />}
+        {tickets.length > 0 && <Charts title={"Priority"} tickets={tickets} />}
       </div>
     </div>
   );
