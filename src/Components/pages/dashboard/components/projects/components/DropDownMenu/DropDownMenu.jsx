a5930d9eb4a0774db15510ea0b19e7f4b7a03133
Jay Ho Oh <ohjayhoi@gmail.com>
2024-04-21 21:42:19 +0900

    refactor: change the name of the prop to make it clear

--- src/Components/pages/dashboard/components/projects/components/DropDownMenu/DropDownMenu.jsx
+++ src/Components/pages/dashboard/components/projects/components/DropDownMenu/DropDownMenu.jsx
@@ -1,13 +1,15 @@
 import "./DropDownMenu.css";
 import axios from "axios";
 
-const DropDownMenu = ({ project_id, setModalOpen }) => {
+const DropDownMenu = ({ project, setModalOpen }) => {
   const handleEdit = async () => {
     setModalOpen(true);
   };
   const handleDelete = async () => {
     try {
-      await axios.delete(`http://localhost:8800/projects/${project_id}`);
+      await axios.delete(
+        `http://localhost:8800/projects/${project.project_id}`
+      );
     } catch (err) {
       console.log(err);
     }
@@ -15,18 +17,20 @@ const DropDownMenu = ({ project_id, setModalOpen }) => {
   };
 
   return (
-    <div className="drop_down_menu_container border_shadow_component">
-      <div className="edit_container">
-        <div className="btn_edit btns_dropdown" onClick={handleEdit}>
-          Edit
+    <>
+      <div className="drop_down_menu_container border_shadow_component">
+        <div className="edit_container">
+          <div className="btn_edit btns_dropdown" onClick={handleEdit}>
+            Edit
+          </div>
+        </div>
+        <div className="delete_container">
+          <div className="btn_delete btns_dropdown" onClick={handleDelete}>
+            Delete
+          </div>
         </div>
       </div>
-      <div className="delete_container">
-        <div className="btn_delete btns_dropdown" onClick={handleDelete}>
-          Delete
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
