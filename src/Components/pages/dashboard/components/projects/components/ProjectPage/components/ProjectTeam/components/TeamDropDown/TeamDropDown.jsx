d8a404a5700d2385fb7aaa8fbcba3b2b4e0f3ed6
Jay Ho Oh <ohjayhoi@gmail.com>
2024-04-22 01:08:34 +0900

    style: move position of the dropdown menu and delete edit button

--- src/Components/pages/dashboard/components/projects/components/ProjectPage/components/ProjectTeam/components/TeamDropDown/TeamDropDown.jsx
+++ src/Components/pages/dashboard/components/projects/components/ProjectPage/components/ProjectTeam/components/TeamDropDown/TeamDropDown.jsx
@@ -15,10 +15,7 @@ const TeamDropDown = ({ member, project_id }) => {
   };
 
   return (
-    <div className="drop_down_menu_container border_shadow_component">
-      <div className="edit_container">
-        <div className="btn_edit btns_dropdown">Edit</div>
-      </div>
+    <div className="team_drop_down_menu_container drop_down_menu_container border_shadow_component">
       <div className="delete_container">
         <div className="btn_delete btns_dropdown" onClick={handleDelete}>
           Delete
